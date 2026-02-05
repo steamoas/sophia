90173e4718191cf9384039e3d3888e7ed79b4c10
steam <steam@ozarkschool.org>
2026-02-05 17:04:29 -0600

    2/5

--- minecart.py
+++ minecart.py
@@ -4,22 +4,23 @@ from robot import drive, left_attachment, right_attachment
 # Authors: Lewis, Jony
 # Setup: 1.2 left
 
+
 async def minecart():
     drive.heading_control.pid(20000, 0, 2500, 3, 7)
     drive.use_gyro(True)
     drive.settings(straight_speed=300)
-    await right_attachment.run_until_stalled(-100,duty_limit=20)
-    await drive.straight(705)
+    # await right_attachment.run_until_stalled(-80, duty_limit=20)
+    await right_attachment.run_angle(75, 20)
+    await drive.straight(750)
     await drive.turn(90)
-    await drive.straight(450)
+    await drive.straight(475)
     await drive.turn(-90)
-    await drive.straight(74)
-
+    await drive.straight(37)
     await multitask(right_attachment.run_angle(180, 200), drive.straight(-16))
-    await drive.straight(6)
-    await right_attachment.run_angle(180,20)
+    await drive.straight(18)
+    await right_attachment.run_angle(180, 20)
     await wait(500)
-    await right_attachment.run_angle(400,-180)
+    await right_attachment.run_angle(400, -180)
     await drive.straight(-61)
     await drive.turn(90)
     await drive.straight(-350)
@@ -27,7 +28,5 @@ async def minecart():
     await drive.straight(-700)
 
 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     run_task(minecart())
