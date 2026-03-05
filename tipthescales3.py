1d1d5a9b37fd5b3ba2e3f24ad28d6cba5ab25025
steamoas <steam@ozarkschool.org>
2026-03-05 13:58:46 -0800

    mason

--- tipthescales3.py
+++ tipthescales3.py
@@ -4,15 +4,16 @@ from pybricks.tools import run_task
 from robot import drive, right_attachment, left_attachment
 
 async def tipthescales3():
-    await drive. straight (260)
+    await drive. straight (265)
     await drive. turn (-90)
-    await drive. straight (524)
+    await drive. straight (526)
     await drive. turn (90)
     await right_attachment.run_angle(60,45)
-    await drive.straight (20)
+    await drive.straight (13)
     await right_attachment.run_angle(60,-45)
-    await drive.straight (-65)
+    await drive.straight (-64)
     await drive.turn (-90)
+    drive.settings (straight_speed=1000)
     await drive.straight (-750)
 if __name__=="__main__":
     run_task(tipthescales3())
